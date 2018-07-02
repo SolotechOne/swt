41df6beb683845674d83dd47a1097f425ea89d7a
c.berberich <c.berberich@c1597nb1420.de.globusgrp.org>
2018-07-02 14:50:07 +0200

    initial commit

--- com.example.e4.rcp/src/com/example/e4/rcp/parts/SamplePart.java
+++ com.example.e4.rcp/src/com/example/e4/rcp/parts/SamplePart.java
@@ -2,6 +2,8 @@ package com.example.e4.rcp.parts;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
@@ -17,11 +19,25 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Text;
+import org.osgi.service.component.annotations.Reference;
+
+import osgi.msg.service.interfaces.MessageReaderService;
 
 public class SamplePart {
 
 	private Text txtInput;
 	private TableViewer tableViewer;
+	
+	private MessageReaderService messageReader;
+	
+	@Reference
+	void addMessageReaderService(MessageReaderService service, Map<String, Object> properties) {
+		this.messageReader=service;
+	}
+
+	void removeMessageReaderService(MessageReaderService service) {
+		this.messageReader=null;
+	}
 
 	@Inject
 	private MDirtyable dirty;
@@ -42,6 +58,8 @@ public class SamplePart {
 
 		tableViewer = new TableViewer(parent);
 
+		System.out.println(messageReader);
+		
 		tableViewer.setContentProvider(ArrayContentProvider.getInstance());;
 		tableViewer.setInput(createInitialDataModel());
 		tableViewer.getTable().setLayoutData(new GridData(GridData.FILL_BOTH));
